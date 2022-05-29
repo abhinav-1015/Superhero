3517d1f307e9707fd3e6f0a1e8e0d74b32ad10d0
abhinav-1015 <103306361+abhinav-1015@users.noreply.github.com>
2022-05-29 09:50:27 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import './App.css';
+import DetailsPage from './pages/DetailsPage';
+// import FavouritePage from './pages/FavouritePage';
+import SearchPage from './pages/SearchPage';
+export default function App() {
+  return (
+   <>
+   <Router>
+   <Switch>
+     <Route path="/" exact component={SearchPage} />
+     <Route path="/superhero/:id" exact component={DetailsPage} />
+     {/* <Route path="/favourites" exact component={FavouritePage} /> */}
+   </Switch>
+   </Router>
+   </>
+  )
+}
